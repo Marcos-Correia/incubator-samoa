a8eaf0ff9cb00aa85a2266c66ccae13e669aa65e
Marcos-Correia <marcos.correia@pucpr.edu.br>
2021-04-13 09:59:40 -0300

    implementing boosting

--- samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/Boosting.java
+++ samoa-api/src/main/java/org/apache/samoa/learners/classifiers/ensemble/Boosting.java
@@ -27,9 +27,11 @@ import org.apache.samoa.core.Processor;
 import org.apache.samoa.instances.Instances;
 import org.apache.samoa.learners.ClassificationLearner;
 import org.apache.samoa.learners.Learner;
-import org.apache.samoa.learners.classifiers.SingleClassifier;
+import org.apache.samoa.learners.classifiers.trees.VerticalHoeffdingTree;
 import org.apache.samoa.topology.Stream;
 import org.apache.samoa.topology.TopologyBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.github.javacliparser.ClassOption;
 import com.github.javacliparser.Configurable;
@@ -42,10 +44,11 @@ public class Boosting implements ClassificationLearner, Configurable {
 
   /** The Constant serialVersionUID. */
   private static final long serialVersionUID = -2971850264864952099L;
+  private static final Logger logger = LoggerFactory.getLogger(Bagging.class);
 
   /** The base learner option. */
   public ClassOption baseLearnerOption = new ClassOption("baseLearner", 'l',
-      "Classifier to train.", Learner.class, SingleClassifier.class.getName());
+      "Classifier to train.", Learner.class, VerticalHoeffdingTree.class.getName());
 
   /** The ensemble size option. */
   public IntOption ensembleSizeOption = new IntOption("ensembleSize", 's',
@@ -54,13 +57,16 @@ public class Boosting implements ClassificationLearner, Configurable {
   /** The distributor processor. */
   private BoostingDistributorProcessor distributorP;
 
+  /** The input streams for the ensemble, one per member. */
+  private Stream[] ensembleStreams;
+
   /** The result stream. */
   protected Stream resultStream;
 
   /** The dataset. */
   private Instances dataset;
 
-  protected Learner classifier;
+  protected Learner[] ensemble;
 
   protected int parallelism;
 
@@ -68,6 +74,7 @@ public class Boosting implements ClassificationLearner, Configurable {
    * Sets the layout.
    */
   protected void setLayout() {
+    int ensembleSize = this.ensembleSizeOption.getValue();
 
     int sizeEnsemble = this.ensembleSizeOption.getValue();
 
@@ -75,9 +82,19 @@ public class Boosting implements ClassificationLearner, Configurable {
     distributorP.setEnsembleSize(sizeEnsemble);
     this.builder.addProcessor(distributorP, 1);
 
-    // instantiate classifier
-    classifier = this.baseLearnerOption.getValue();
-    classifier.init(builder, this.dataset, sizeEnsemble);
+    ensemble = new Learner[ensembleSize];
+    for (int i = 0; i < ensembleSize; i++) {
+      try {
+        ensemble[i] = (Learner) ClassOption.createObject(baseLearnerOption.getValueAsCLIString(),
+                baseLearnerOption.getRequiredType());
+      } catch (Exception e) {
+        logger.error("Unable to create members of the ensemble. Please check your CLI parameters");
+        e.printStackTrace();
+        throw new IllegalArgumentException(e);
+      }
+      ensemble[i].init(builder, this.dataset, 1); // sequential
+    }
+
 
     BoostingPredictionCombinerProcessor predictionCombinerP = new BoostingPredictionCombinerProcessor();
     predictionCombinerP.setEnsembleSize(sizeEnsemble);
@@ -87,23 +104,20 @@ public class Boosting implements ClassificationLearner, Configurable {
     resultStream = this.builder.createStream(predictionCombinerP);
     predictionCombinerP.setOutputStream(resultStream);
 
-    for (Stream subResultStream : classifier.getResultStreams()) {
-      this.builder.connectInputKeyStream(subResultStream, predictionCombinerP);
+    for (Learner member : ensemble) {
+      for (Stream subResultStream : member.getResultStreams()) { // a learner can have multiple output streams
+        this.builder.connectInputKeyStream(subResultStream, predictionCombinerP); // the key is the instance id to combine predictions
+      }
     }
 
-    /* The testing stream. */
-    Stream testingStream = this.builder.createStream(distributorP);
-    this.builder.connectInputKeyStream(testingStream, classifier.getInputProcessor());
+    ensembleStreams = new Stream[ensembleSize];
+    for (int i = 0; i < ensembleSize; i++) {
+      ensembleStreams[i] = builder.createStream(distributorP);
+      builder.connectInputShuffleStream(ensembleStreams[i], ensemble[i].getInputProcessor()); // connect streams one-to-one with ensemble members (the type of connection does not matter)
+    }
 
-    /* The prediction stream. */
-    Stream predictionStream = this.builder.createStream(distributorP);
-    this.builder.connectInputKeyStream(predictionStream, classifier.getInputProcessor());
+    distributorP.setOutputStreams(ensembleStreams);
 
-    // Addition to Bagging: stream to train
-    /* The training stream. */
-    Stream trainingStream = this.builder.createStream(predictionCombinerP);
-    predictionCombinerP.setTrainingStream(trainingStream);
-    this.builder.connectInputKeyStream(trainingStream, classifier.getInputProcessor());
 
   }
 
@@ -121,7 +135,6 @@ public class Boosting implements ClassificationLearner, Configurable {
   public void init(TopologyBuilder builder, Instances dataset, int parallelism) {
     this.builder = builder;
     this.dataset = dataset;
-    this.parallelism = parallelism;
     this.setLayout();
   }
 
